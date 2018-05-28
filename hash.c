0718c948cccca21abbfdad496440c1caf600a047
Will Xu <will@glowing.com>
2018-05-28 08:13:01 +0000

    handle unicode hash

--- hash.c
+++ hash.c
@@ -1,21 +1,51 @@
 #include <Python.h>
 
-static PyObject* hash(PyObject *self, PyObject *args) {
-    PyObject *object;
+static long
+hash_unicode(PyObject *obj) {
     register Py_ssize_t len;
-    register Py_UNICODE *p;
     register long x = 0;
-    if (!PyArg_ParseTuple(args, "O:obj", &object)) {
-        return NULL;
-    }
-    len = PyUnicode_GET_SIZE(object);
-    p = PyUnicode_AS_UNICODE(object);
+    register Py_UNICODE *p;
+    len = PyUnicode_GET_SIZE(obj);
+    if (len == 0) return 0;
+    p = PyUnicode_AS_UNICODE(obj);
     x ^= *p << 7;
     while (--len >= 0) {
         x = (1000003*x) ^ *p++;
     }
-    x ^= PyUnicode_GET_SIZE(object);
-    return Py_BuildValue("l", x);
+    x ^= PyUnicode_GET_SIZE(obj);
+    if (x == -1) return -2;
+    return x;
+}
+
+static long
+hash_bytes(PyObject *obj) {
+    register Py_ssize_t len;
+    register unsigned char *p;
+    register long x = 0;
+    len = Py_SIZE(obj);
+    if (len == 0) return 0;
+    p = (unsigned char *) ((PyBytesObject *)obj) -> ob_sval;
+    x ^= *p << 7;
+    while (--len >= 0)
+        x = (1000003*x) ^ *p++;
+    x ^= Py_SIZE(obj);
+    if (x == -1) return -2;
+    return x;
+}
+
+
+static PyObject* hash(PyObject *self, PyObject *args) {
+    PyObject *object;
+    if (!PyArg_ParseTuple(args, "O:obj", &object)) {
+        return NULL;
+    }
+    if (PyBytes_Check(object)) {
+        return Py_BuildValue("l", hash_bytes(object));
+    } else if (PyUnicode_Check(object)) {
+        return Py_BuildValue("l", hash_unicode(object));
+    }
+    PyErr_Format(PyExc_TypeError, "object must bytes or unicode, got %s", object->ob_type->tp_name);
+    return NULL;
 }
 
 PyDoc_STRVAR(pydoc_hash,
